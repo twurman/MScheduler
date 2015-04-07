71bddb234162bbb0ac272c56b00e2cd7ba2511f4
Tim W <twurman@umich.edu>
2015-04-07 16:30:26 -0400

    updated base template

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ from flask import render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['api_url'] = "//umich-schedule-api.herokuapp.com"
+app.secret_key = 'test'
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, jsonify
+from flask import render_template, session, request, jsonify
 from app import app
 from schedule_api import *
 
@@ -9,6 +9,29 @@ def index():
     options['terms'] = get_terms()
 
     return render_template('index.html', **options)
+
+@app.route('/backpack_course/<term>/<school>/<subject>/<course_num>/<course_title>')
+def backpack_course(term, school, subject, course_num, course_title):
+
+	if not 'backpack' in session:
+		backpack = []
+		session['backpack'] = backpack
+
+	backpack_item = {}
+
+	backpack_item['term'] = term
+	backpack_item['school'] = school
+	backpack_item['subject'] = subject
+	backpack_item['course_num'] = course_num
+	backpack_item['course_title'] = course_title
+	# backpack_item['course_description'] = get_course_description(term, school, subject, course_num)
+	# backpack_item['sections'] = get_sections(term, school, subject, course_num)
+
+	session['backpack'].append(backpack_item)
+	print session['backpack']
+
+	options= {}
+	return render_template('index.html', **options)
 
 @app.route('/course_description/<term>/<school>/<subject>/<course_num>/<course_title>')
 def course_description(term, school, subject, course_num, course_title):
