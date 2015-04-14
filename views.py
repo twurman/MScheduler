159d2281e6ac6b387148c634066e6e89927996d7
Adam Schnitzer <adamschnitzer@gmail.com>
2015-04-14 16:04:28 -0400

    add mock for get_scheds

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from flask import render_template, session, request, jsonify
 from app import app
 from schedule_api import *
+from datetime import datetime
+from datetime import timedelta
 
 @app.route('/')
 def index():
@@ -19,61 +21,83 @@ def index():
 @app.route('/backpack_course/<term>/<school>/<subject>/<course_num>')
 def backpack_course(term, school, subject, course_num):
 
-	if not 'backpack' in session:
-		backpack = []
-		session['backpack'] = backpack
+    if not 'backpack' in session:
+        backpack = []
+        session['backpack'] = backpack
 
-	backpack_item = {}
-	backpack_item['term'] = term
-	backpack_item['school'] = school
-	backpack_item['subject'] = subject
-	backpack_item['course_num'] = course_num
-	# backpack_item['course_title'] = course_title
-	# backpack_item['course_description'] = get_course_description(term, school, subject, course_num)
-	# backpack_item['sections'] = get_sections(term, school, subject, course_num)
+    backpack_item = {}
+    backpack_item['term'] = term
+    backpack_item['school'] = school
+    backpack_item['subject'] = subject
+    backpack_item['course_num'] = course_num
+    # backpack_item['course_title'] = course_title
+    # backpack_item['course_description'] = get_course_description(term, school, subject, course_num)
+    # backpack_item['sections'] = get_sections(term, school, subject, course_num)
 
-	inBackpack = False;
-	for item in session['backpack']:
-		if item['course_num'] == backpack_item['course_num'] and item['subject'] == backpack_item['subject'] and item['school'] == backpack_item['school']:
-			inBackpack = True
-	if not inBackpack:
-		session['backpack'].append(backpack_item)
-	print session['backpack']
+    inBackpack = False;
+    for item in session['backpack']:
+        if item['course_num'] == backpack_item['course_num'] and item['subject'] == backpack_item['subject'] and item['school'] == backpack_item['school']:
+            inBackpack = True
+    if not inBackpack:
+        session['backpack'].append(backpack_item)
+    print session['backpack']
 
-	options= {}
-	return render_template('index.html', **options)
+    options= {}
+    return render_template('index.html', **options)
+
+@app.route('/get_schedules')
+def get_scheds():
+    current_week = datetime.today()
+    current_week.replace(day=(current_week.day - current_week.weekday()) )
+    test_events = [
+        {
+            'id': 0,
+            'title': 'Class 1',
+            'allDay': False,
+            'start': current_week.isoformat(),
+            'end': (current_week + timedelta(hours=1)).isoformat(),
+        },
+        {
+            'id': 1,
+            'title': 'Class 2',
+            'allDay': False,
+            'start': (current_week + timedelta(days=1)).isoformat(),
+            'end': (current_week + timedelta(days=1,hours=1)).isoformat(),
+        },
+    ]
+    return json.dumps([ test_events, test_events, test_events ])
 
 @app.route('/course_description/<term>/<school>/<subject>/<course_num>/<course_title>')
 def course_description(term, school, subject, course_num, course_title):
 
-	options = {}
+    options = {}
 
-	options['term'] = term
-	options['school'] = school
-	options['subject'] = subject
-	options['course_num'] = course_num
-	options['course_title'] = course_title
-	options['course_description'] = get_course_description(term, school, subject, course_num)
-	options['sections'] = get_sections(term, school, subject, course_num)
+    options['term'] = term
+    options['school'] = school
+    options['subject'] = subject
+    options['course_num'] = course_num
+    options['course_title'] = course_title
+    options['course_description'] = get_course_description(term, school, subject, course_num)
+    options['sections'] = get_sections(term, school, subject, course_num)
 
-	return render_template('course_description.html', **options)
+    return render_template('course_description.html', **options)
 
 @app.route('/section/<term>/<school>/<subject>/<course_num>/<course_title>/<section_num>')
 def section(term, school, subject, course_num, course_title, section_num):
 
-	options = {}
+    options = {}
 
-	options['term'] = term
-	options['school'] = school
-	options['subject'] = subject
-	options['course_num'] = course_num
-	options['course_title'] = course_title
-	options['course_description'] = get_course_description(term, school, subject, course_num)
-	options['section'] = section_num
-	options['section_details'] = get_section_details(term, school, subject, course_num, section_num)
-	options['meetings'] = get_meetings(term, school, subject, course_num, section_num)
-	options['textbook'] = get_textbook(term, school, subject, course_num, section_num)
-	options['instructors'] = get_instructors(term, school, subject, course_num, section_num)
+    options['term'] = term
+    options['school'] = school
+    options['subject'] = subject
+    options['course_num'] = course_num
+    options['course_title'] = course_title
+    options['course_description'] = get_course_description(term, school, subject, course_num)
+    options['section'] = section_num
+    options['section_details'] = get_section_details(term, school, subject, course_num, section_num)
+    options['meetings'] = get_meetings(term, school, subject, course_num, section_num)
+    options['textbook'] = get_textbook(term, school, subject, course_num, section_num)
+    options['instructors'] = get_instructors(term, school, subject, course_num, section_num)
 
-	return render_template('section.html', **options)
+    return render_template('section.html', **options)
 
